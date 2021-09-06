6a14c1d2e33c81f8d02fe4f15935666721c293d2
Mark Hähnel <hello@markhaehnel.de>
2021-09-06 14:25:14 +0200

    Remove whitelist UI paging and make list scrollable

--- Web/src/components/WhitelistList.tsx
+++ Web/src/components/WhitelistList.tsx
@@ -5,33 +5,27 @@ import { Card } from "./Card";
 import { Loader } from "./Loader";
 
 export function WhitelistList() {
-    const ITEMS_PER_PAGE = 5;
-    const [page, setPage] = useState(0);
     const {
         data: users,
         error,
         isFetching,
     } = useQuery("whitelist", async () => {
-        setPage(0);
         let data = await fetchWhitelist();
         return data?.data?.users.sort((a: string, b: string) =>
             a.toLowerCase().localeCompare(b.toLowerCase())
         );
     });
-    const pageStart = page * ITEMS_PER_PAGE;
-    const pageEnd = pageStart + ITEMS_PER_PAGE;
-    const maxPages = users ? Math.ceil(users.length / ITEMS_PER_PAGE) : 0;
 
     return (
         <Card>
-            <div className="h-[324px]">
+            <div className="">
                 <div className="flex flex-row m-4">
                     <div className="text-xl">Whitelist</div>
                     <div className="flex-grow"></div>
                     {isFetching && <Loader />}
                 </div>
 
-                <div className="flex flex-col">
+                <div className="flex flex-col overflow-y-scroll h-[320px]">
                     {error && (
                         <div className="text-md py-2 px-4">
                             Error while fetching whitelist.
@@ -39,35 +33,16 @@ export function WhitelistList() {
                     )}
                     {!error && users && (
                         <div className="flex flex-col">
-                            {users
-                                .slice(pageStart, pageEnd)
-                                .map((user: string) => {
-                                    return (
-                                        <div
-                                            className="text-md odd:bg-gray-100 py-2 px-4 hover:bg-gray-200"
-                                            key={user}
-                                        >
-                                            {user}
-                                        </div>
-                                    );
-                                })}
-
-                            <div className="flex flex-row mx-4 mt-4 gap-2">
-                                {Array.from(Array(maxPages).keys()).map(
-                                    (_, i) => {
-                                        return (
-                                            <button
-                                                disabled={page === i}
-                                                onClick={() => setPage(i)}
-                                                key={i}
-                                                className="text-center text-lg text-white bg-blue-600 border-2 border-transparent px-2 rounded-md shadow cursor-pointer hover:bg-blue-500 transition-transform active:bg-blue-600 active:scale-95 focus:border-gray-600 disabled:bg-blue-300 disabled:cursor-not-allowed;"
-                                            >
-                                                {i + 1}
-                                            </button>
-                                        );
-                                    }
-                                )}
-                            </div>
+                            {users.map((user: string) => {
+                                return (
+                                    <div
+                                        className="text-md odd:bg-gray-100 py-2 px-4 hover:bg-gray-200"
+                                        key={user}
+                                    >
+                                        {user}
+                                    </div>
+                                );
+                            })}
                         </div>
                     )}
                 </div>
