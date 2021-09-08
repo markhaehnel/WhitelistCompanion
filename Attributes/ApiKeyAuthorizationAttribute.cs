4375e28778d28661fbe05c6f03f4e3b52344d358
Mark Hähnel <hello@markhaehnel.de>
2021-09-08 14:37:14 +0200

    Refactor and cleanup of ApiKeyAuthorizationAttribute

--- Attributes/ApiKeyAuthorizationAttribute.cs
+++ Attributes/ApiKeyAuthorizationAttribute.cs
@@ -1,45 +1,53 @@
 using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using WhitelistCompanion.Configuration;
+using WhitelistCompanion.Models;
 
 namespace WhitelistCompanion.Attributes
 {
     [AttributeUsage(validOn: AttributeTargets.Class)]
-    public class ApiKeyAuthorizationAttribute : Attribute, IAsyncActionFilter
+    public sealed class ApiKeyAuthorizationAttribute : Attribute, IAsyncActionFilter
     {
-        private const string APIKEYNAME = "ApiKey";
         public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
-            if (!context.HttpContext.Request.Headers.TryGetValue(APIKEYNAME, out var extractedApiKey))
-            {
-                context.Result = new ContentResult()
-                {
-                    ContentType = "application/json",
-                    StatusCode = 401,
-                    Content = $"{{ \"success\": false, \"error\": \"{APIKEYNAME} was not provided\" }}"
-                };
-                return;
-            }
+            if (context is null) throw new ArgumentNullException(nameof(context));
+            if (next is null) throw new ArgumentNullException(nameof(next));
 
             var apiConfig = context.HttpContext.RequestServices.GetRequiredService<IOptions<ApiConfiguration>>().Value;
             var apiKey = apiConfig.Key;
 
-            if (!apiKey.Equals(extractedApiKey))
+            var hasApiKey = context.HttpContext.Request.Headers.TryGetValue(Constants.ApiKeyHeaderName, out var extractedApiKey);
+
+            if (!hasApiKey)
             {
-                context.Result = new ContentResult()
-                {
-                    ContentType = "application/json",
-                    StatusCode = 401,
-                    Content = $"{{ \"success\": false, \"error\": \"{APIKEYNAME} invalid\" }}"
-                };
+                context.Result = BuildResult($"{Constants.ApiKeyHeaderName} was not provided");
+                return;
+            }
+
+            if (!extractedApiKey.Equals(apiKey))
+            {
+                context.Result = BuildResult($"{Constants.ApiKeyHeaderName} was not provided");
                 return;
             }
 
             await next();
+        }
+
+        private static JsonResult BuildResult(string errorMessage)
+        {
+            var result = new JsonResult(new ApiResponse<object>()
+            {
+                Error = errorMessage
+            })
+            {
+                StatusCode = StatusCodes.Status401Unauthorized
+            };
+            return result;
         }
     }
 }
